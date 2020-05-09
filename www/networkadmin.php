b2cb0224355f8b827d7f218e3f620a91e0b93bbd
kevinvu184 <kevinvu184@gmail.com>
2020-05-09 15:04:03 +1000

    Network Admin Front End

--- www/networkadmin.php
+++ www/networkadmin.php
@@ -15,6 +15,10 @@ $datastore = new DatastoreClient();
 $teamName = array();
 $score = array();
 
+$querySlot = $datastore->query();
+$querySlot->kind('slot');
+$slots = $datastore->runQuery($querySlot);
+
 $query = $datastore->query();
 $query->kind('team');
 $queryUser = $datastore->query();
@@ -29,6 +33,7 @@ foreach ($teams as $team) {
         array_push($score,0);
     }
 }
+
 $jsTeamNameArray = json_encode($teamName);
 $jsScoreArray = json_encode($score);
 
@@ -68,54 +73,144 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="shortcut icon" href="/favicon.svg">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src='script.js'></script>
 </head>
 
 <body class="bg-light">
     <div class="container-sm py-4 my-5 bg-dark text-white rounded-lg">
-        <canvas id="myChart" aria-label="Hello ARIA World" role="img"></canvas>
-        <script>
-            var teamName= <?php echo $jsTeamNameArray ?>;
-            var teamScore= <?php echo $jsScoreArray ?>;
-            let myChart = document.getElementById('myChart').getContext('2d');
-            Chart.defaults.global.defaultFontSize=18;
-            Chart.defaults.global.defaultFontColor='white';
-            let massPopChart = new Chart(myChart, {
-                type: 'bar',
-                data: {
-                    labels: teamName,
-                    datasets: [{
-                        label:'Mark',
-                        data: teamScore,
-                        backgroundColor: '#66C7F4',
-                        borderWidth: 1,
-                        borderColor: '#eee',
-                        hoverBorderWidth: 3,
-                        barPercentage: 0.8,
-                        barThickness: 30,
-                        maxBarThickness: 40,
-                        minBarLength: 2,
-                        hoverBorderColor: '#fff'
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                suggestedMin: 0,
-                                suggestedMax: 10,
+        <div >
+            <div class="row">
+                <div class="col">
+                    <button class="tablink btn btn-primary btn-lg btn-block" style="background-color: green;" onclick="openPage('News', this, 'green')" id="defaultOpen">Slot</button>
+                </div>
+                <div class="col">
+                    <button class="tablink btn btn-primary btn-lg btn-block" onclick="openPage('Home', this, 'green')">Chart</button>
+                </div>
+            </div>
+            
+            <div id="Home" class="tabcontent rounded border border-white my-3" style="display: none;">
+                <canvas id="myChart" aria-label="Hello ARIA World" role="img"></canvas>
+                <script>
+                    var teamName= <?php echo $jsTeamNameArray ?>;
+                    var teamScore= <?php echo $jsScoreArray ?>;
+                    let myChart = document.getElementById('myChart').getContext('2d');
+                    Chart.defaults.global.defaultFontSize=18;
+                    Chart.defaults.global.defaultFontColor='white';
+                    let massPopChart = new Chart(myChart, {
+                        type: 'bar',
+                        data: {
+                            labels: teamName,
+                            datasets: [{
+                                label:'Mark',
+                                data: teamScore,
+                                backgroundColor: '#66C7F4',
+                                borderWidth: 1,
+                                borderColor: '#eee',
+                                hoverBorderWidth: 3,
+                                barPercentage: 0.8,
+                                barThickness: 30,
+                                maxBarThickness: 40,
+                                minBarLength: 2,
+                                hoverBorderColor: '#fff'
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        suggestedMin: 0,
+                                        suggestedMax: 10,
+                                        fontColor: '#fff'
+                                    }
+                                }]
+                            },
+                            title: {
+                                display: true,
+                                text: 'RMIT NETWORK PROGRAMMING DEMO SCORE',
+                                fontSize: 25,
                                 fontColor: '#fff'
                             }
-                        }]
-                    },
-                    title: {
-                        display: true,
-                        text: 'RMIT NETWORK PROGRAMMING DEMO SCORE',
-                        fontSize: 25,
-                        fontColor: '#fff'
-                    }
-                }
-            });
-        </script>
+                        }
+                    });
+                </script>
+            </div>
+            
+            <div id="News" class="tabcontent rounded border border-white my-3">
+                <h3 class="text-center">SLOT STATUS</h3>
+                <table class="table table-dark table-hover">
+                    <thead>
+                        <tr class="bg-info">
+                            <th scope="col">#</th>
+                            <th scope="col">Team name</th>
+                            <th scope="col">Demo Date</th>
+                            <th scope="col">Demo Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $slotCounter = 0; ?>
+                        <?php foreach($slots as $slot): ?>
+                        <tr class="bg-light text-dark">
+                            <th scope="row"> echo $slotCounter++</th>
+                            <td> echo $slot['DateAndTime']</td>
+                            <td> echo $slot['DateAndTime']</td>
+                            <td> echo$slot['Status']</td>
+                        </tr>
+                        <?php endforeach; ?>
+                </table>
+            </div>
+        </div>
+        
+        <div class="mb-2">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary btn-lg btn-block" data-toggle="modal" data-target="#addNewSlotModal">
+                Add new slot
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="addNewSlotModal" tabindex="-1" role="dialog" aria-labelledby="addNewSlotModalTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content text-dark">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">New Slot</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="#" method="POST">
+                                <div class="form-group">
+                                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Day</label>
+                                    <select name="day" class="custom-select my-1 mr-sm-2" id="daySelect">
+                                        <script>seedDate()</script>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Hour</label>
+                                    <select name="hour" class="custom-select my-1 mr-sm-2" id="hourSelect">
+                                        <option selected>Choose hour...</option>
+                                        <option>09:00</option>
+                                        <option>10:00</option>
+                                        <option>11:00</option>
+                                        <option>14:00</option>
+                                        <option>15:00</option>
+                                        <option>16:00</option>
+                                        <option>17:00</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-success">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <form action="#" method="POST">
             <input type="submit" name="reset" class="btn btn-warning btn-lg btn-block" value="Reset">
             <input type="submit" name="back" class="btn btn-danger btn-lg btn-block" value="Log out">
